220cf52bd1b2ac07cfd01c7977b2494116967a5e
Amanda Roscoe <aroscoe@corpu.com>
2016-07-26 16:38:01 -0400

    modified partial file path

--- app/config.js
+++ app/config.js
@@ -11,56 +11,56 @@ angular
       sliderImage: '/images/radio_buttons.png',
       description: 'Single Choice',
       editOptions: ['options'],
-      viewTemplate: '../app/partials/singleChoice.html',
+      viewTemplate: './app/partials/singleChoice.html',
       displayPreferenceOptions: [ 'radio', 'dropdown']
     },
     multipleChoiceListChooseOne: {
       sliderImage: '/images/radio_buttons_grid.png',
       description: 'Radio Button Grid',
       editOptions: ['options','rows'],
-      viewTemplate: '../app/partials/multipleChoiceListChooseOne.html',
+      viewTemplate: './app/partials/multipleChoiceListChooseOne.html',
       displayPreferenceOptions: []
     },
     multipleChoice: {
       sliderImage: '/images/checkbox.png',
       description: 'Multiple Choice',
       editOptions: ['options'],
-      viewTemplate: '../app/partials/multipleChoice.html',
+      viewTemplate: './app/partials/multipleChoice.html',
       displayPreferenceOptions: ['checkbox']
     },
     textAnswer: {
       sliderImage: '/images/textbox.png',
       description: 'Text Answer',
       editOptions:[],
-      viewTemplate: '../app/partials/textAnswer.html',
+      viewTemplate: './app/partials/textAnswer.html',
       displayPreferenceOptions: ['short', 'long']
     },
     number: {
       sliderImage: '/images/number.png',
       description: 'Number',
       editOptions:['scaleFormat'],
-      viewTemplate: '../app/partials/number.html',
+      viewTemplate: './app/partials/number.html',
       displayPreferenceOptions: ['textbox', 'slider']
     },
     likertScale: {
       sliderImage: '/images/likerScale.png',
       description: 'Likert Scale (1-7)',
       editOptions:['scaleLabels'],
-      viewTemplate: '../app/partials/likertScale.html',
+      viewTemplate: './app/partials/likertScale.html',
       displayPreferenceOptions:[]
     },
     netPromoterScore: {
       sliderImage: '/images/nps.png',
       description: 'NPS (0-10)',
       editOptions:[],
-      viewTemplate: '../app/partials/nps.html',
+      viewTemplate: './app/partials/nps.html',
       displayPreferenceOptions:[]
     },
     ranking: {
       sliderImage: '/images/ranking.png',
       description: 'Ranking',
       editOptions:['rows'],
-      viewTemplate: '../app/partials/ranking.html',
+      viewTemplate: './app/partials/ranking.html',
       displayPreferenceOptions: ['dragAndDrop', 'rankingGrid']
     }
   }
